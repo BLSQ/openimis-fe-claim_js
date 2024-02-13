cf313faa076f5e55ae18919ce9c67b30686ad061
Thibault Dethier <tdethier@bluesquarehub.com>
2024-02-13 15:25:17 +0100

    Fixed review and form save bugs
    Fixed the claim item/service status bug during review: now it's possible to approve/reject individual items/services.
    Fixes the claim form bug for valuated claims: now it's impossible to edit a claim once it has been approved.

--- src/components/ClaimForm.js
+++ src/components/ClaimForm.js
@@ -182,6 +182,7 @@ class ClaimForm extends Component {
       }
       if (!items.length && !services.length) return !!this.canSaveClaimWithoutServiceNorItem;
     }
+    if (this.state.claim.status > 4) return false;
     return true;
   };
 
